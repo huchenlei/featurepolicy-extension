8aa09942792513c715e9fda68c31010121ddfa0d
Charlie Hu <chenleihu@google.com>
2020-10-09 15:54:10 -0400

    :white_check_mark: add tests on policy header classes; :bug: fix various bugs

--- background.js
+++ background.js
@@ -52,9 +52,9 @@ class FeaturePolicyHeader {
     return new FeaturePolicyHeader(header_string.split(';')
       .reduce((acc, item) => {
         const [policyName, ...allowlist] = item.trim().split(' ').filter(s => s !== '');
-        acc[policyName] = allowlist;
+        acc.set(policyName, allowlist);
         return acc;
-      }, Map()));
+      }, new Map()));
   }
 
   serialize() {
@@ -76,25 +76,26 @@ class PermissionsPolicyHeader {
    *
    * Note: Permissions-Policy header uses Dictionary in [structured header syntax]
    *(https://httpwg.org/http-extensions/draft-ietf-httpbis-header-structure.html).
-  *
-  * Note: This function does not provide full structured header syntax parsing.
-  * When incomplete syntax is received, it can lead to unexpected behaviour.
-  *
-  * @param {string} header_string e.g. p1=(), p2=("foo.com" "bar.com"), p3=self
-  * @returns {PermissionsPolicyHeader}
-  */
+   *
+   * Note: This function does not provide full structured header syntax parsing.
+   * When incomplete syntax is received, it can lead to unexpected behaviour.
+   *
+   * @param {string} header_string e.g. p1=(), p2=("foo.com" "bar.com"), p3=self
+   * @returns {PermissionsPolicyHeader}
+   */
   static parse(header_string) {
     return new PermissionsPolicyHeader(header_string.split(',')
       .reduce((acc, item) => {
-        const [policyName, allowlist_string] = item.split('=').filter(s => s !== '');
-        const match_result = allowlist_string.match(/\((.*)\)/g);
+        const [policyName, allowlist_string] = item.split('=').map(s => s.trim());
+        const match_result = allowlist_string.match(/\((.*)\)/);
         // Bracket is optional when there is only single item in SH's dictionary
         // value.
-        const allowlist = match_result ?
-          match_result[1].split(' ') : [allowlist_string]
-        acc[policyName] = allowlist;
+        const allowlist = match_result ? (
+          match_result.length > 1 ? match_result[1].split(' ') : ''
+        ) : [allowlist_string];
+        acc.set(policyName, allowlist);
         return acc;
-      }, Map()));
+      }, new Map()));
   }
 
   serialize() {
@@ -103,7 +104,6 @@ class PermissionsPolicyHeader {
       .join(', ');
   }
 };
-
 
 /**
  * Override 'Permissions-Policy' and 'Feature-Policy' header 's value
@@ -156,47 +156,64 @@ function overridePolicies(policyManager, headers) {
   };
 }
 
-/**
- * Note: the DevTools network panel won 't show these modified response headers
- * but the changes will work.The panel shows the original headers as returned
- * from the network.See https: //crbug.com258064.
- * TODO: figure out how to communicate this to users.It 'll be confusing
- * if they check the DevTools and it doesn 't show correct values.
- */
-chrome.webRequest.onHeadersReceived.addListener(details => {
-  // If it is not the top-frame, we just ignore it.
-  if (details.frameId !== 0 || !connections.has(details.tabId)) {// || tab.id !== details.tabId) {
-    return;
+// Export node module for testing.
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+  module.exports = {
+    FeaturePolicyHeader,
+    PermissionsPolicyHeader,
+    overridePolicies
   }
-  const policyManager = connections.get(details.tabId).policyManager;
-  return overridePolicies(policyManager, details.responseHeaders);
-}, {urls: ['<all_urls>'], types: ['main_frame']}, ['blocking', 'responseHeaders']);
+}
 
-chrome.runtime.onConnect.addListener(port => {
-
-  const extensionListener = (message, sender, sendResponse) => {
-    // The original connection event doesn't include the tab ID of the
-    // DevTools page, so we need to send it explicitly.
-    switch (message.name) {
-      case 'init':
-        connections.set(message.tabId, {port, policyManager: null});
-        break;
-      default:
-        // noop
+if (typeof chrome !== 'undefined') {
+  /**
+   * Note: the DevTools network panel won 't show these modified response headers
+   * but the changes will work.The panel shows the original headers as returned
+   * from the network.See https: //crbug.com258064.
+   * TODO: figure out how to communicate this to users.It 'll be confusing
+   * if they check the DevTools and it doesn 't show correct values.
+   */
+  chrome.webRequest.onHeadersReceived.addListener(details => {
+    // If it is not the top-frame, we just ignore it.
+    if (details.frameId !== 0 || !connections.has(details.tabId)) { // || tab.id !== details.tabId) {
+      return;
     }
-  };
+    const policyManager = connections.get(details.tabId).policyManager;
+    return overridePolicies(policyManager, details.responseHeaders);
+  }, {
+    urls: ['<all_urls>'],
+    types: ['main_frame']
+  }, ['blocking', 'responseHeaders']);
 
-  port.onMessage.addListener(extensionListener); // Listen to messages sent from the DevTools page.
+  chrome.runtime.onConnect.addListener(port => {
 
-  port.onDisconnect.addListener(port => {
-    port.onMessage.removeListener(extensionListener);
-
-    for (const [tabId, val] of connections) {
-      if (val.port === port) {
-        connections.get(tabId).policyManager = null; // TODO: make sure to not leave retaining objects around...?
-        connections.delete(tabId);
-        break;
+    const extensionListener = (message, sender, sendResponse) => {
+      // The original connection event doesn't include the tab ID of the
+      // DevTools page, so we need to send it explicitly.
+      switch (message.name) {
+        case 'init':
+          connections.set(message.tabId, {
+            port,
+            policyManager: null
+          });
+          break;
+        default:
+          // noop
       }
-    }
+    };
+
+    port.onMessage.addListener(extensionListener); // Listen to messages sent from the DevTools page.
+
+    port.onDisconnect.addListener(port => {
+      port.onMessage.removeListener(extensionListener);
+
+      for (const [tabId, val] of connections) {
+        if (val.port === port) {
+          connections.get(tabId).policyManager = null; // TODO: make sure to not leave retaining objects around...?
+          connections.delete(tabId);
+          break;
+        }
+      }
+    });
   });
-});
+}
---
+++ spec/background.spec.js
@@ -0,0 +1,45 @@
+const { FeaturePolicyHeader, PermissionsPolicyHeader } = require('../background.js');
+
+describe('FeaturePolicyHeader', () => {
+  it('Should parse valid policy', () => {
+    expect(FeaturePolicyHeader.parse(
+      "A 'self';  B *; C 'none'; D foo.com bar.com 'self'"))
+      .toEqual(new FeaturePolicyHeader(new Map(Object.entries({
+        A: ["'self'"],
+        B: ["*"],
+        C: ["'none'"],
+        D: ["foo.com", "bar.com", "'self'"]
+      }))));
+  });
+
+  it('Should serialize correctly', () => {
+    expect(new FeaturePolicyHeader(new Map(Object.entries({
+      A: ["'self'"],
+      B: ["*"],
+      C: ["'none'"],
+      D: ["foo.com", "bar.com", "'self'"]
+    }))).serialize()).toEqual("A 'self'; B *; C 'none'; D foo.com bar.com 'self'");
+  });
+});
+
+describe('PermissionsPolicyHeader', () => {
+  it('Should parse valid policy', () => {
+    expect(PermissionsPolicyHeader.parse(
+      'A=self,  B=*, C=(), D=("foo.com" "bar.com" self)'))
+      .toEqual(new PermissionsPolicyHeader(new Map(Object.entries({
+        A: ["self"],
+        B: ["*"],
+        C: [""],
+        D: ['"foo.com"', '"bar.com"', "self"]
+      }))));
+  });
+
+  it('Should serialize correctly', () => {
+    expect(new PermissionsPolicyHeader(new Map(Object.entries({
+      A: ["self"],
+      B: ["*"],
+      C: [""],
+      D: ['"foo.com"', '"bar.com"', "self"]
+    }))).serialize()).toEqual('A=(self), B=(*), C=(), D=("foo.com" "bar.com" self)');
+  });
+});
